e61a250ea86c45cc5d28989d0c6ac9c75c830f69
joshmoots15-design <joshmoots15@gmail.com>
2026-04-14 12:18:37 -0500

    Started My Own cpp Proj.

---
+++ cpp project/pro.cpp
@@ -0,0 +1,56 @@
+// Josh Moots Spr26 Coding 1 cpp Project.
+// for windows, it's 'cl /EHsc proj.cpp and type proj'
+
+// create a .cpp program that will allow the user to 
+// add, edit, remove, and show all games (sorted), and then quit
+// use vectors, iters, and algors to do so
+// pseidocode to organize your thoughts
+// show available comms to the user at the top of each loop
+//  the games do not to save between launches of the program
+// the user should type the command they want to use as a word
+        // First I should put out the available commands
+            //"add", "remove", "random", "sort", "rate", "quit" 
+        // then ask for the player to list games
+        // display commands again
+        // "add" adds games to the list
+        // "remove" gets rid a game from the list
+        // "random" mixes up the list
+        // "sort" orders the list alphbetically
+        // "rate" randomly picks a game from the list and 
+        // then will ask you to rate it out of ten.
+        // "quit" leaves the game
+
+
+#include <iostream>
+#include <string>           // for getline()
+#include <vector>           // for vectors and iterators
+#include <algorithm>        // for find(), sort(), random_shuffle()
+
+using namespace std;
+
+int main() {
+    cout << "Im ready to do stuff and things!\n";
+
+    do{
+        cout << "Here are your options:\n";
+        cout << "'add', 'remove', 'random', 'sort', 'rate', or 'quit'.\n";
+
+        string input;
+
+        getline (cin, input);
+
+        if(input == "add") {
+            cout << "You may now start adding games! Type 'done' when finished!\n";
+        }
+
+    } while (true);
+
+
+
+
+
+
+
+    return 0;
+}
+
--- wk2/vec2.cpp
+++ wk2/vec2.cpp
@@ -1,89 +1,155 @@
-// Josh Moots Coding 1 SPR26
+// Josh Moots Coding 1 Spr26
 // Vectors, Iterators, and Algorithms
 
-//'cl /EHsc vec.cpp' is compile
-//'vec' to activate
+// for windows, it's 'cl /EHsc vec.cpp && vec'
+// for macOS, it's 'g++ vec.cpp -o vec && ./vec'
+
+// create a .cpp program that will allow the user to 
+// add, edit, remove, and show all games (sorted), and then quit
+// use vectors, iters, and algors to do so
+// pseidocode to organize your thoughts
+// show available comms to the user at the top of each loop
+//  the games do not to save between launches of the program
+// the user should type the command they want to use as a word
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <string>           // for getline()
+#include <vector>           // for vectors and iterators
+#include <algorithm>        // for find(), sort(), random_shuffle()
 
 using namespace std;
 
-int main(){
-    cout << "I'm working!!!\n";
+int main() {
+    cout << "Let's learn about vectors!\n";
 
+    vector<string> favGames = {"Hollow Knight", "Silksong", "Sol Cesto"};
     string input;
+    // int numberInput = stoi(input);
 
-   do{
-    cout << "What do you want?\n";
-        cout << "If you want to, you can type 'push'.";
 
-        getline(cin, input);
+    do {
+        cout << "What would you like to do?\n";
+        cout << "You can type 'quit', 'push', 'find', 'remove'.\n";
 
-     if(input == "push") {
-            cout << "lets get working on vector of strings gang!\n";
+        getline(cin, input);        // get input from the player
+
+        if(input == "push") {
+            cout << "\nLet's make our first vector of strings.\n";
 
             vector<string> names;
 
-            names.push_back("Hornet");
-            names.push_back("Lil Ghost");
-            names.push_back("Quirrel");
-            names.push_back("The Knight");
-            names.push_back("Grimm");
+            // add some names with names.push_back()
+            names.push_back("Edric Muller");
+            names.push_back("Ishmael Rozario");
+            names.push_back("Faylinn Graeves");
+            names.push_back("Martyr Long");
+            names.push_back("Henry Quetzal");
 
             cout << "here are your names!\n";
-            for(int i =0; i < names.size(); i++){
-                cout << names[i] << "\n";
+            for(int i = 0; i < names.size(); i++) {
+                cout << "    " << names[i] << "\n";
             }
-        }
-            else if(input == "find"){
-                cout << "lets try to use the find algorithm.\n";
 
-                vector<string> favs = {"Hollow Knight", "Silksong", "Nine Sols"};
+            cout << "There are " << names.size() << " names.\n\n";
 
-                vector<string>::iterator iter;
+            cout << *(names.end() - 1) << " is sour. Let's get rid of him.\n";
+            
+            names.pop_back();       // pop_back() removes the last element from the vector
+            
+            cout << "here are your names!\n";
+            for(int i = 0; i < names.size(); i++) {
+                cout << "    " << names[i] << "\n";
+            }
 
-                cout << "here are your favs!\n";
-                for(int i = 0; i < favs.size(); i++) {
-                    cout << "   " << favs[i] << "\n";
-                }
 
-                iter = favs.begin();
+        } // end of 'push'
+        else if(input == "find") {
+            cout << "Let's try to use the find algorithm.\n";
 
-                cout << "iter is pointing at " << *iter << ".\n";
+            // using a collection initializer to add favs at creation of vector.
+            vector<string> favs = {"Bells of Bloodwryte", "No Strings Attached", "Sink or Swim"};
 
-                iter += 1;
+            // building an iterator. it points to a 'spot' in a vector.
+            vector<string>::iterator iter;
 
-                cout << "Now iter is pointing at " << *iter << ".\n";
+            cout << "here are your favs!\n";
+            for(int i = 0; i < favs.size(); i++) {
+                cout << "    " << favs[i] << "\n";
+            }
 
-                cout << "what name would you like to select?\n";
+            // make iter point at the first element in the vector
+            iter = favs.begin(); 
 
+            // the asterisk is a "dereference operator"... so that we can get 
+                // the value that the iter is pointing at.
+            cout << "iter is pointing at " << *iter << ".\n";
+
+            iter += 1;
+
+            cout << "Now iter is pointing at " << *iter << ".\n";
+
+            cout << "What name would you like to select?\n";
+
+            getline(cin, input);
+            // use the find algorithm
+            iter = find(favs.begin(), favs.end(), input);
+            
+            if(iter != favs.end()) {
+                cout << "We've found " << *iter << "!\n";
+                cout << "Would you like to change this fav?\n";
                 getline(cin, input);
 
-                find(favs.begin(), favs.end(), "Silksong");
-
-                if(iter != favs.end()) {
-                    cout << "We've found " << *iter << "!\n";
-                    cout << "Would you like to change this fav?\n";
+                if (input == "yes") {
+                    cout << "What would you like to change this fav to?\n";
                     getline(cin, input);
 
-                    *iter = input
+                    *iter = input;      // changing the fav.
                 }
 
-                
-                else {
-                    cout << "We couldnt find that name.\n";
+                cout << "here are your updated favs!\n";
+                for(int i = 0; i < favs.size(); i++) {
+                    cout << "    " << favs[i] << "\n";
                 }
             }
-         else if(input == "quit") {
-            cout << "ok bye, loser jeez.\n";
+            else {
+                cout << "We couldn't find that name.\n";
+            }
+
+        }
+        else if(input == "quit") {
+            cout << "Thanks for playing.\n";
             break;
         }
 
-        else{
-            cout << "try again idiot.\n";
+        else if (input == "remove") {
+
+            sort(favGames.begin(), favGames.end());
+
+            cout << "Here are your favorite games:\n";
+
+            for(int i = 0; i < favGames.size(); i++){
+                cout << favGames[i] << endl;
+            }
+
+            cout << "What game would you like to remove from the list?\n";
+            getline(cin, input);
+
+            auto iter = find(favGames.begin(), favGames.end(), input);
+
+            if(iter != favGames.end()) {
+                cout << "We have found the name, we getting rid of it!\n";
+                favGames.erase(iter);
+            }
+
+    
         }
-   }
+        else {
+            cout << "I didn't recognise that command.\n";
+        }
+
+    } while(true);
+
+    // create a new vector of strings
+
+    return 0;
 }
